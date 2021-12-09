dd6e38fcc96260bc10edc1aa135da294554ec9d1
hrishika-alaj <81599734+hrishika-alaj@users.noreply.github.com>
2021-12-09 19:03:27 +1000

    Commented koalas ABM

--- koalas_with_space.jl
+++ koalas_with_space.jl
@@ -3,78 +3,95 @@ using Agents, Random
 using InteractiveDynamics
 using CairoMakie
 using Plots
+using Statistics
 
+
+#Define koala/Eucalyptus agent struct
 mutable struct KoalaOrEucalyptus <: AbstractAgent
-    id::Int
-    pos::NTuple{2,Int}
+    id::Int #agent id
+    pos::NTuple{2,Int} #agent's position on GridSpace
     type::Symbol #:koala or :Eucalyptus
     death_prob::Float64 #only for Koala
     production_rate::Float64 #only for eucalyptus
     consume_rate::Float64 #only for eucalyptus
-    #reproduction prob for later
 end
 
+#define Koala and Eucalyptus agents for easier use
 Koala(id,pos,death_prob,production_rate, consume_rate) = KoalaOrEucalyptus(id, pos, :koala, death_prob, production_rate, consume_rate)
 Eucalyptus(id, pos, death_prob,production_rate, consume_rate) = KoalaOrEucalyptus(id, pos, :eucalyptus, death_prob, production_rate, consume_rate)
 
 
-
+#initalise model with space and agents
 function initialize_model(;
-    n_koala = 10,
-    n_eucalyptus = 20,
-    koala_death_rate = 0.005,
-    eucalyptus_production_rate = 0.008,
-    eucalyptus_consume_rate = 0.04,
-    seed = 23182,
+    n_koala = 10, #initial koala population
+    n_eucalyptus = 20, #initial eucalyptus population
+    koala_death_rate = 0.0025, #death rate of koala agents
+    eucalyptus_production_rate = 0.008, #prduction rate of eucalyptus
+    eucalyptus_consume_rate = 0.08, #death/consumption rate of eucalyptus
+    seed = 23182, #seed for model
      )
-     space = GridSpace((20, 20); periodic = true)
+     space = GridSpace((20, 20); periodic = true) #initalise grid space with periodic boundary conditions
+     #Define properties of model for easier referencing
      properties = Dict(:euca_pr => eucalyptus_production_rate, :koala_dr => koala_death_rate, :euca_cr => eucalyptus_consume_rate)
-     #properties = ()
 
-     rng = MersenneTwister(seed)
+     rng = MersenneTwister(seed) #rng for model
+     #define the Koalas ABM with the propeties, space and rng
      model = ABM(KoalaOrEucalyptus, space; properties,rng, scheduler = Schedulers.randomly)
 
-     id = 0;
+     id = 0; #initialise for agents
 
+     #add initial number of koalas to model
      for _ in 1:n_koala
          id += 1
-         koala = Koala(id,(1, 1),koala_death_rate, 0,0)
+         koala = Koala(id,(1, 1),koala_death_rate, 0,0) #define Koala agent with approriate properties
          add_agent_single!(koala, model) #add agent to unoccupied position
-         #add_agent!(koala, model)
      end
-
+     #add initial number of eucalyptus to model
      for _ in 1:n_eucalyptus
          id += 1
+         #define Eucalyptus agent with approriate properties
          eucalyptus = Eucalyptus(id, (1, 1), 0, eucalyptus_production_rate, eucalyptus_consume_rate)
-         add_agent_single!(eucalyptus, model)
-         #add_agent!(eucalyptus, model)
+         add_agent_single!(eucalyptus, model) #add agent to unoccupied position
      end
 
-     return model
+     return model #return the initalised model
  end
 
+#helper functions to count amount koala and eucalyptus agents
 leaf(m) = count(a.type == :eucalyptus for a in allagents(m))
 koalas(m) = count(a.type == :koala for a in allagents(m))
 
-
+#agent step that executes for every time step
  function agent_step!(agent::KoalaOrEucalyptus,model)
-     # num_euc = leaf(model)
-     # if num_euc < 1
-     #     eucalyptus_step!(agent,model)
+     #if the current agent is a koala, then execute the koala step function, else
+     # execute the eucalytpus step
      if agent.type == :koala
          koala_step!(agent,model)
     else
         eucalyptus_step!(agent,model)
     end
 
+    #current number of koala and eucalyptus agents
     num_euca = leaf(model)
     num_koalas = koalas(model)
 
+    #if there are no eucalytpus agents in simulation, then
+    #based on the production rate, add an eucalyptus agent to sim
     if num_euca == 0
         if rand() < model.euca_pr
             id = nextid(model)
             eucalyptus_new = Eucalyptus(id, (1, 1), 0, model.euca_pr, model.euca_cr)
-            add_agent_single!(eucalyptus_new, model)
+            add_agent_single!(eucalyptus_new, model) #add agent to unoccupied position
+        end
+    end
+
+    #if there are no koala agents in simulation, then
+    #based on the eucalyptus consumption rate, add a koala agent to sim
+    if num_koalas == 0
+        if rand() < model.euca_cr
+            id = nextid(model)
+            k_new = Koala(id,(1, 1), model.koala_dr, 0,0)
+            add_agent_single!(k_new, model)
         end
     end
 
@@ -82,11 +99,12 @@ koalas(m) = count(a.type == :koala for a in allagents(m))
 
  end
 
+#Defines the behaviour of koala agents
  function koala_step!(koala,model)
-     #decide the agent's movement based on prob
+     #decide the agent's movement on grid based on random walk
      upper_bound = 0; #right and left bound
      lower_bound = 0; #up and down bound
-     prob_walk = rand()
+     prob_walk = rand() #generate a random number
      if prob_walk < 0.25 #north so move up 1 and down 0
          upper_bound = 0; #no left or right movement
          lower_bound = 1; #move up 1
@@ -104,36 +122,36 @@ koalas(m) = count(a.type == :koala for a in allagents(m))
     #move agent based on bounds  -> ifempty may need to be false
      walk!(koala, (upper_bound,lower_bound), model; ifempty = true)
 
+
+     num_koalas = koalas(model) #current num koalas
+     num_eucalyptus = leaf(model) # current num of eucalyptus
+
      # search nearby for eucalyptus -> nearby agents
      # then based on prob, consume it and then kill that agent
      #make new koala at random space
-     num_koalas = koalas(model) #num koalas
-     num_eucalyptus = leaf(model) #num of eucalyptus
-     # prob_contact = num_koalas/(num_eucalyptus + num_koalas) #determine prob of contact
-     # if rand() < prob_contact
          if rand() < model.euca_cr
              for neighbor in nearby_agents(koala, model)
                  if neighbor.type == :eucalyptus #if neighbour is eucalyptus
                      kill_agent!(neighbor,model) #the eucalyptus gets eaten
                      id = nextid(model)
                      koala_new = Koala(id,(1, 1), model.koala_dr, 0,0)
-                     add_agent_single!(koala_new, model) #add agent to unoccupied position #new koala gets added to model
+                     add_agent_single!(koala_new, model) #new koala gets added to model
                      break #breaks after 1 iteration (only consume 1)
                  end
              end
          end
 
 
-     #after, we can also kill the agent based on prob
+     #after, chance of killng  the agent based on prob
      if rand() < koala.death_prob
          kill_agent!(koala,model)
      end
 
  end
 
+#Defines the behaviour of eucalyptus agents
 function eucalyptus_step!(eucalyptus,model)
-    #can't move so never moves
-    #add agent in position and then put in random for now
+    #agent has no spatial movements
     new_euca = leaf(model) #calculate num of eucalyptus
     if rand() < eucalyptus.production_rate*(1-(new_euca)/1000)
                 #new eucalyptus if the above condition is met
@@ -150,11 +168,13 @@ end
  koala(a) = a.type == :koala
  eucalyptus(a) = a.type == :eucalyptus
 
+#running model with 2000 time steps
  model = initialize_model()
  n_steps = 2000
  adata = [(koala, count), (eucalyptus, count)]
  adf, _ = run!(model, agent_step!, dummystep, n_steps; adata)
 
+#function to plot one simulation of model
  function plot_population_timeseries(adf)
      figure = Figure(resolution = (600, 400))
      ax = figure[1, 1] = Axis(figure; xlabel = "Step", ylabel = "Population")
@@ -166,18 +186,13 @@ end
 
  plot_population_timeseries(adf)
 
-
-
- # model_1 = initialize_model();
- # agent_50_df, _ = run!(model_1, agent_step!, nsteps; adata = to_collect);
- # agent_50_df[!,:t] = t;
+## Running 1000 simulations + plots
 
  a_koalas = adf[:,2];
  a_euca = adf[:,3];
 
- # 151 element vector
 
- for i in 2:50
+ for i in 2:10
      model_2 = initialize_model()
      n_step = 2000;
      agent_df_2, _ = run!(model_2, agent_step!, dummystep,n_step; adata);
@@ -187,25 +202,108 @@ end
      global a_euca = [a_euca a_2]
  end
 
- # to_remove = [];
- #
- # for j in 1:size(a_50)[2]
- #     if (sum(a_50[:,j]) <= 300)
- #         push!(to_remove,j)
- #     end
- # end
- #
- # filt_a_50 = a_50[:, setdiff(1:end, (to_remove))]
- # mean_of_50 = mean(filt_a_50, dims = 2)
+ #Plots.histogram(a_euca[2001,:], bins = 20)
 
- Plots.plot(adf.step, a_koalas[:,1], legend = :none, title = "ABM results for 50 runs",xlab="Time step",ylabel="Number", linecolor = :red)
-  Plots.plot!(adf.step, a_euca[:,1], legend = :none, title = "ABM results for 50 runs",xlab="Time step",ylabel="Number", linecolor = :green)
+ mean_of_50 = mean(a_koalas, dims = 2)
+ median_of_50 = median(a_koalas, dims = 2)
+ std_of_50 = std(a_koalas, dims = 2)
+
+ mean_of_50_e = mean(a_euca, dims = 2)
+ median_of_50_e = median(a_euca, dims = 2)
+ std_of_50_e = std(a_euca, dims = 2)
+
+
+#all plots
+ Plots.plot(adf.step, a_koalas[:,1],  label = "", title = "ABM populations from 1000 runs with K = 20, E = 10",xlab="Time step",ylabel="Number", linecolor = :red, titlefontsize = 8)
+ Plots.plot!(adf.step, a_euca[:,1], label = "",xlab="Time step",ylabel="Number", linecolor = :blue)
  for i in 2:(size(a_koalas)[2]-1)
      #Plots.plot!(t, a_50[:,i])#, seriestype = :scatter)
-     Plots.plot!(adf.step, a_koalas[:,i], linecolor = :red)
-     Plots.plot!(adf.step, a_euca[:,i], linecolor = :green)
+     Plots.plot!(adf.step, a_koalas[:,i], linecolor = :red, label = "")
+     Plots.plot!(adf.step, a_euca[:,i], linecolor = :blue, label = "")
 
  end
  #Plots.plot!(t,a_50[:,50])#, seriestype = :scatter)
- Plots.plot!(adf.step, a_koalas[:,size(a_koalas)[2]], linecolor = :red)
- Plots.plot!(adf.step, a_euca[:,size(a_koalas)[2]], linecolor = :green)
+ Plots.plot!(adf.step, a_koalas[:,size(a_koalas)[2]], label = "Koala", linecolor = :red)
+ Plots.plot!(adf.step, a_euca[:,size(a_koalas)[2]],label = "Eucalyptus", linecolor = :blue, legend = :top, legendfontsize = 6)
+
+ Plots.plot!(adf.step, mean_of_50, label = "Mean (Koalas)", lw = 3, linecolor = :black)
+ Plots.plot!(adf.step, 2*std_of_50 + mean_of_50, label = "2+ std (Koalas)", lw = 3, linecolor = :black, linestyle = :dash )
+ Plots.plot!(adf.step, abs.(mean_of_50 - 2*std_of_50) , label = "2- std(Koalas)", lw = 3.5, linecolor = :black, linestyle = :dot )
+
+ Plots.plot!(adf.step, mean_of_50_e, label = "Mean (Euca.)", lw = 3, linecolor = :green1)
+ Plots.plot!(adf.step, 2*std_of_50_e + mean_of_50_e, label = "2+ std (Euca.)", lw = 3,linecolor = :green1, linestyle = :dash)
+ Plots.plot!(adf.step, abs.(mean_of_50_e - 2*std_of_50_e) , label = "2- std(Euca.)", lw = 3.5,linecolor = :green1, linestyle = :dot)
+
+
+
+
+
+## simultion
+# model12 = initialize_model()
+# offset(a) = a.type == :koala ? (-0.7, -0.5) : (-0.3, -0.5)
+# ashape(a) = a.type == :koala ? :circle : :utriangle
+# acolor(a) = a.type == :koala ? RGBAf0(0.2, 0.2, 0.2, 0.8) : RGBAf0(0.22, 0.58, 0.2, 0.8)
+#
+# plotkwargs = (
+#     ac = acolor,
+#     as = 15,
+#     am = ashape,
+#     offset = offset,
+# )
+#
+# fig, _ = abm_plot(model12; plotkwargs...)
+# fig
+
+#
+# # n_steps = 2000
+# # adata = [(koala, count), (eucalyptus, count)]
+# # adf2, _ = run!(model1, agent_step!, dummystep, n_steps; adata)
+# #
+# # plot_population_timeseries(adf2)
+#
+# abm_video(
+#     "koalaeuca.mp4",
+#     model1,
+#     agent_step!;
+#     frames = 200,
+#     framerate = 8,
+#     plotkwargs...,
+# )
+
+
+## average behaviour
+
+# mean_of_50 = mean(a_koalas, dims = 2)
+# median_of_50 = median(a_koalas, dims = 2)
+# std_of_50 = std(a_koalas, dims = 2)
+#
+# mean_of_50_e = mean(a_euca, dims = 2)
+# median_of_50_e = median(a_euca, dims = 2)
+# std_of_50_e = std(a_euca, dims = 2)
+#
+# Plots.plot(adf.step, mean_of_50, label = "Mean", lw = 2,title = "Koala results from 1000 runs", xlab="Time",ylabel="Number",legend = :outertopright)
+# Plots.plot!(adf.step, median_of_50, label = "Median", lw = 2)
+# Plots.plot!(adf.step, 2*std_of_50 + mean_of_50, label = "2+ std", lw = 2)
+# Plots.plot!(adf.step, abs.(mean_of_50 - 2*std_of_50) , label = "2- std", lw = 2)
+# Plots.plot(adf.step, mean_of_50_e, label = "Mean", lw = 2,title = "Eucalyptus results from 1000 runs", xlab="Time",ylabel="Number",legend = :outertopright)
+# Plots.plot!(adf.step, median_of_50_e, label = "Median", lw = 2)
+# Plots.plot!(adf.step, 2*std_of_50_e + mean_of_50_e, label = "2+ std", lw = 2)
+# Plots.plot!(adf.step, abs.(mean_of_50_e - 2*std_of_50_e) , label = "2- std", lw = 2)
+
+##
+
+# leftplot = subplot(1,2,1)
+# Plots.plot(adf.step, mean_of_50, label = "Mean", lw = 2,title = "Results from 150 runs", xlab="Time",ylabel="Number",legend = :best)
+# Plots.plot!(adf.step, median_of_50, label = "Median", lw = 2)
+# Plots.plot!(adf.step, 2*std_of_50 + mean_of_50, label = "2+ std", lw = 2)
+# Plots.plot!(adf.step, abs.(mean_of_50 - 2*std_of_50) , label = "2- std", lw = 2)
+# rightplot = subplot(1,2,2)
+# Plots.plot(adf.step, mean_of_50_e, label = "Mean", lw = 2, linestyle = :dash,title = "Eucalyptus results from 150 runs", xlab="Time",ylabel="Number",legend = :topleft)
+# Plots.plot!(adf.step, median_of_50_e, label = "Median", lw = 2, linestyle = :dash)
+# Plots.plot!(adf.step, 2*std_of_50_e + mean_of_50_e, label = "2+ std", lw = 2, linestyle = :dash)
+# Plots.plot!(adf.step, abs.(mean_of_50_e - 2*std_of_50_e) , label = "2- std", lw = 2, linestyle = :dash)
+#
+# fig = gcf()
+# title!(fig, "Nice plot")         # Same as `title("Nice plot")`
+# title!(leftplot, "First plot")  # Set the title of the first plot
+# fig   # The function `title!` does not update the visualization
